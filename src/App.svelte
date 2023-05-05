a6ae3f8f161a9fb4d1dd58f3c7ea576dc59b5d16
KPic <kevinpichon@outlook.fr>
2023-05-05 15:14:38 +0000

    Choix du nombre de lettres

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
+  import LettersCountInput from "./components/LettersCountInput.svelte";
 
+  let step = 0
+  let lettersCount = null
+  
+  const handleLettersCountInputChange = (value: number) => {
+    lettersCount = value
+    step = 1
+  }
 </script>
 
 <main>
-
+  {#if step === 0}
+    <LettersCountInput on:change={handleLettersCountInputChange} />
+  {/if}
 </main>
 
 <style>
-
+ main {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+ }
 </style>
---
+++ src/components/LettersCountInput.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  const handleClick = (value: number) => {
+    dispatch('change', value);
+  }
+</script>
+
+<div>
+  <h2>Nombre de lettres</h2>
+  <div class="buttons">
+    <button class="large" on:click={() => handleClick(5)}>5</button>
+    <button class="large" on:click={() => handleClick(6)}>6</button>
+    <button class="large" on:click={() => handleClick(7)}>7</button>
+    <button class="large" on:click={() => handleClick(8)}>8</button>
+    <button class="large" on:click={() => handleClick(9)}>9</button>
+  </div>
+</div>
+
+<style>
+  .buttons {
+    display: flex;
+    justify-content: center;
+    gap: 12px;
+  }
+</style>
